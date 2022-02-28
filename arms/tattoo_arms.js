68add42115759a5f158e2debde90a41350c12c8d
cy4309 <cy4309@gmail.com>
2022-03-01 07:52:32 +0800

    add.RWDexcept_apmt

---
+++ arms/tattoo_arms.js
@@ -0,0 +1,217 @@
+//換圖
+var jarpro =[
+    {
+        tt:'./img/Arms/1.png', //修改
+        tt_jam:'./img/Arms/6.png',  //修改
+        tt_bake:'img/toastbake.png',
+        tt_bake_jam:'img/toastbake_jam01.png',
+
+        tt_mb:'img/toast_mb.png',
+        tt_jam_mb:'./img/Arms/6.png', //修改
+        tt_bake_mb:'img/toastbake_mb.png',
+        tt_bake_jam_mb:'img/toastbake_jam01_mb.png',
+
+        // tit:'��㕑�𤘪�𣈯��',
+        // in:'��𣈯�祆�𣂼�嚗朞�㕑�瓐���蒾��蝟硔��瑼豢狀��憭拍�嗆�烐�䀹�𡏭��'
+    },
+    {
+        tt:'./img/Arms/1.png', //修改
+        tt_jam:'./img/Arms/4.png',  //修改
+        tt_bake:'img/toastbake.png',
+        tt_bake_jam:'./img/Arms/4.png',  //修改
+
+        tt_mb:'img/toast_mb.png',
+        tt_jam_mb:'img/toast_jam02_mb.png',
+        tt_bake_mb:'img/toastbake_mb.png',
+        tt_bake_jam_mb:'img/toastbake_jam02_mb.png',
+
+        // tit:'�蓡擐蹱�𨀣�𣈯��',
+        // in:'��𣈯�祆�𣂼�嚗𡁶蓡擐蹱�栶���蒾��蝟硔��瑼豢狀��憭拍�嗆�烐�䀹�𡏭��'
+    },
+    {
+        tt:'./img/Arms/1.png', //修改
+        tt_jam:'./img/Arms/3.png', //修改
+        tt_bake:'img/toastbake.png',
+        tt_bake_jam:'img/toastbake_jam12.png',
+
+        tt_mb:'img/toast_mb.png',
+        tt_jam_mb:'img/toast_jam12_mb.png',
+        tt_bake_mb:'./img/Arms/3.png', //修改
+        tt_bake_jam_mb:'img/toastbake_jam12_mb.png',
+
+        // tit:'��𧢲�𡏭�𠰴���𣈯��',
+        // in:'��𣈯�祆�𣂼�嚗朞�𧢲�栶����𠰴����蒾��蝟硔��瑼豢狀��憭拍�嗆�烐�䀹�𡏭��'
+    },
+    {
+        tt:'./img/Arms/1.png', //修改
+        tt_jam:'./img/Arms/5.png', //修改
+        tt_bake:'img/toastbake.png',
+        tt_bake_jam:'img/toastbake_jam03.png',
+
+        tt_mb:'./img/Arms/1.png', //修改
+        tt_jam_mb:'./img/Arms/5.png', //修改
+        tt_bake_mb:'img/toastbake_mb.png',
+        tt_bake_jam_mb:'img/toastbake_jam03_mb.png',
+
+        // tit:'憟�㺭��𨀣�𣈯��',
+        // in:'��𣈯�祆�𣂼�嚗𡁜��㺭��栶���蒾��蝟硔��瑼豢狀��憭拍�嗆�烐�䀹�𡏭��'
+    },
+    {
+        tt:'./img/Arms/1.png', //修改
+        tt_jam:'./img/Arms/2.png', //修改
+        tt_bake:'img/toastbake.png',
+        tt_bake_jam:'img/toastbake_jam08.png',
+
+        tt_mb:'img/toast_mb.png',
+        tt_jam_mb:'./img/Arms/2.png', //修改
+        tt_bake_mb:'img/toastbake_mb.png',
+        tt_bake_jam_mb:'img/toastbake_jam08_mb.png',
+
+        // tit:'獢烐允��𣈯��',
+        // in:'��𣈯�祆�𣂼�嚗𡁏�烐允���蒾��蝟硔��瑼豢狀��憭拍�嗆�烐�䀹�𡏭��'
+    },
+    // {
+    //     tt:'img/toast.png',
+    //     tt_jam:'img/toast_jam04.png',
+    //     tt_bake:'img/toastbake.png',
+    //     tt_bake_jam:'img/toastbake_jam04.png',
+
+    //     tt_mb:'img/toast_mb.png',
+    //     tt_jam_mb:'img/toast_jam04_mb.png',
+    //     tt_bake_mb:'img/toastbake_mb.png',
+    //     tt_bake_jam_mb:'img/toastbake_jam04_mb.png',
+
+    //     tit:'�𠆩璅���𥕦扒��𣈯��',
+    //     in:'��𣈯�祆�𣂼�嚗朞𠆩璅�����𥕦扒���蒾��蝟硔��瑼豢狀��憭拍�嗆�烐�䀹�𡏭��'
+    // },
+    // {
+    //     tt:'img/toast.png',
+    //     tt_jam:'img/toast_jam10.png',
+    //     tt_bake:'img/toastbake.png',
+    //     tt_bake_jam:'img/toastbake_jam10.png',
+
+    //     tt_mb:'img/toast_mb.png',
+    //     tt_jam_mb:'img/toast_jam10_mb.png',
+    //     tt_bake_mb:'img/toastbake_mb.png',
+    //     tt_bake_jam_mb:'img/toastbake_jam10_mb.png',
+
+    //     tit:'��埝�𣈯陶璇冽�𣈯��',
+    //     in:'��𣈯�祆�𣂼�嚗朞�埝�栶��曈單◢���蒾��蝟硔��瑼豢狀��憭拍�嗆�烐�䀹�𡏭��'
+    // },
+    // {
+    //     tt:'img/toast.png',
+    //     tt_jam:'img/toast_jam07.png',
+    //     tt_bake:'img/toastbake.png',
+    //     tt_bake_jam:'img/toastbake_jam07.png',
+
+    //     tt_mb:'img/toast_mb.png',
+    //     tt_jam_mb:'img/toast_jam07_mb.png',
+    //     tt_bake_mb:'img/toastbake_mb.png',
+    //     tt_bake_jam_mb:'img/toastbake_jam07_mb.png',
+
+    //     tit:'��𧢲�𨀣�烐�䀹�𣈯��',
+    //     in:'��𣈯�祆�𣂼�嚗朞�𧢲�栶��璈睃�僐���蒾��蝟硔��瑼豢狀��憭拍�嗆�烐�䀹�𡏭��'
+    // },
+    // {
+    //     tt:'img/toast.png',
+    //     tt_jam:'img/toast_jam09.png',
+    //     tt_bake:'img/toastbake.png',
+    //     tt_bake_jam:'img/toastbake_jam09.png',
+
+    //     tt_mb:'img/toast_mb.png',
+    //     tt_jam_mb:'img/toast_jam09_mb.png',
+    //     tt_bake_mb:'img/toastbake_mb.png',
+    //     tt_bake_jam_mb:'img/toastbake_jam09_mb.png',
+
+    //     tit:'��滩�𤘪�𣈯��',
+    //     in:'��𣈯�祆�𣂼�嚗朞�滩�瓐���蒾��蝟硔��瑼豢狀��憭拍�嗆�烐�䀹�𡏭��'
+    // },
+    // {
+    //     tt:'img/toast.png',
+    //     tt_jam:'img/toast_jam13.png',
+    //     tt_bake:'img/toastbake.png',
+    //     tt_bake_jam:'img/toastbake_jam13.png',
+
+    //     tt_mb:'img/toast_mb.png',
+    //     tt_jam_mb:'img/toast_jam13_mb.png',
+    //     tt_bake_mb:'img/toastbake_mb.png',
+    //     tt_bake_jam_mb:'img/toastbake_jam13_mb.png',
+
+    //     tit:'蝝�樴齿�𨀣�𣈯��',
+    //     in:'��𣈯�祆�𣂼�嚗𡁶�樴齿�栶���蒾��蝟硔��瑼豢狀��憭拍�嗆�烐�䀹�𡏭��'
+    // },
+    
+    
+];
+
+
+
+//換圖
+function jarclick(count,item,normal_tt){
+    return function(){
+        if(is_normal){
+            document.getElementById('tt').srcset = item.tt;
+            document.getElementById('tt_jam').srcset = item.tt_jam;
+            document.getElementById('tt_mb').srcset = item.tt_mb;
+            document.getElementById('tt_jam_mb').srcset = item.tt_jam_mb;
+            //瘝垍�日�𡒊���𣂼虬
+        }else{
+            document.getElementById('tt').srcset = item.tt_bake;
+            document.getElementById('tt_jam').srcset = item.tt_bake_jam;
+            document.getElementById('tt_mb').srcset = item.tt_bake_mb;
+            document.getElementById('tt_jam_mb').srcset = item.tt_bake_jam_mb;
+            //�日�𡒊���𣂼虬
+        };
+    document.getElementById('tit').innerText = item.tit;
+    document.getElementById('in').innerHTML = item.in;
+    SelectCount=count;
+    };
+};
+
+//換圖
+function toastclick(item,is_normal){
+    if(is_normal){
+        document.getElementById('tt').srcset = item.tt;
+        document.getElementById('tt_jam').srcset = item.tt_jam;
+        document.getElementById('tt_mb').srcset = item.tt_mb;
+        document.getElementById('tt_jam_mb').srcset = item.tt_jam_mb;
+        //瘝垍�日�𡒊���𣂼虬
+    }else{
+        document.getElementById('tt').srcset = item.tt_bake;
+        document.getElementById('tt_jam').srcset = item.tt_bake_jam;
+        document.getElementById('tt_mb').srcset = item.tt_bake_mb;
+        document.getElementById('tt_jam_mb').srcset = item.tt_bake_jam_mb;
+        //�日�𡒊���𣂼虬
+    };
+    document.getElementById('tit').innerText = item.tit;
+    document.getElementById('in').innerHTML = item.in;
+};
+
+var is_normal = true; //�蹱糓瘝垍�日��
+var SelectCount = 0; //�蹱糓�豢��洵撟曄𣂎��𣈯�穿�屸�鞱身0�箇洵銝��𣂎
+
+function bakeclick(){
+    is_normal = false;
+    toastclick(jarpro[SelectCount],is_normal);
+};
+
+function normalclick(){
+    is_normal = true;
+    toastclick(jarpro[SelectCount],is_normal);
+};
+
+
+var baked = document.querySelector('#bake');
+if(baked)
+baked.addEventListener('click',bakeclick,false); 
+
+var baked1 = document.querySelector('#normal');
+if(baked1)
+baked1.addEventListener('click',normalclick,false); 
+
+var jars = document.querySelectorAll('.jar li');
+for( let i=0 ; i<jars.length ;i++){
+    jars[i].addEventListener('click',jarclick(i,jarpro[i],is_normal),false);
+};
+
+
